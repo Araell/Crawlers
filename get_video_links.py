9e4f1c4da7d22aee516559b57edf9ee9f99e8222
Araell <araell@outlook.com>
2016-02-19 10:28:20 +0800

    #

--- get_video_links.py
+++ get_video_links.py
@@ -5,12 +5,14 @@ import re
 import sys
 urlopen = urllib2.urlopen
 blogname = sys.argv[1]
-video_links_file = open(blogname + '.txt', 'w+')
+# a 以追加模式打开文件（即一打开文件，文件指针自动移到文件末尾），如果文件不存在则创建
+# w+ 消除文件内容，然后以读写方式打开文件
+video_links_file = open(blogname + '.txt', 'a')
 
 
 def get_content_from_url(url):  # 从url中读取页面内容
     attempts = 0
-    content = ''
+    content = None
     while attempts < 10:
         print 'get_content_from_url -- cur attempts : ', attempts
         try:
@@ -26,11 +28,12 @@ def extract_video_links(result):
     for item in result:
         print item
         video_page = get_content_from_url(item)
-        video_link = re.findall(
-            '(http[s]*://www.tumblr.com/video_file/[\S]+)[\"|\']', video_page, re.M | re.S)
-        print video_link
-        if len(video_link) > 0:
-            video_links_file.write(video_link[0] + '\n')
+        if video_page is not None:
+            video_link = re.findall(
+                '(http[s]*://www.tumblr.com/video_file/[\S]+)[\"|\']', video_page, re.M | re.S)
+            print video_link
+            if len(video_link) > 0:
+                video_links_file.write(video_link[0] + '\n')
 
 
 base_url = 'http://' + blogname + '.tumblr.com/page/'
@@ -39,12 +42,13 @@ while True:
     url = base_url + str(page)
     print(url)
     content = get_content_from_url(url)
-    result = re.findall(
-        '(http[s]*://www.tumblr.com/video/[\S]+)\'', content, re.M | re.S)
-    if len(result) == 0:
-        break
-    else:
-        page = page + 1
-        extract_video_links(result)
+    if content is not None:
+        result = re.findall(
+            '(http[s]*://www.tumblr.com/video/[\S]+)\'', content, re.M | re.S)
+        if len(result) == 0:
+            break
+        else:
+            page = page + 1
+            extract_video_links(result)
 
 video_links_file.close()
